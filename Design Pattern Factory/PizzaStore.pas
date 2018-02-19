5c7aa4b7e34a5e7cfe2bce1863d2f85b1b0ef13d
GabrielBevilacqua <gabriel.scavassa97@gmail.com>
2018-02-19 16:06:11 -0300

    Duvidas

--- Design Pattern Factory/PizzaStore.pas
+++ Design Pattern Factory/PizzaStore.pas
@@ -16,6 +16,12 @@ type
     class function PedirPizza(Pizzas: string): TPizza;
   end;
 
+  TPedido = class
+    class var FPizzas: TArray<TPizza>;
+    class procedure Pedir(Qtde: Integer; const Tipo: string);
+    class procedure MostrarPedido;
+  end;
+
 implementation
 
 { TPizzaStore }
@@ -24,14 +30,36 @@ class function TPizzaStore.PedirPizza(Pizzas: string): TPizza;
 var
   ClasseDaPizza: TTipoDePizzas;
 begin
+  Result := nil;
   for ClasseDaPizza in CardapioPizzas do
     if ClasseDaPizza.ClassName = Pizzas then
       Result := (ClasseDaPizza.Create); //perguntar sobre isso
+
+  if Result = nil then
+    raise Exception.Create('Pizza não cadastrada.');
 end;
 
 class procedure TPizzaStore.RegistrarPizza(Pizza: TTipoDePizzas);
 begin
   CardapioPizzas := CardapioPizzas + [Pizza];
+end;
+
+{ TPedido }
+
+class procedure TPedido.MostrarPedido;
+var
+  P: TPizza;
+begin
+  for P in FPizzas do
+    WriteLn(P.PrepararPizza);
+end;
+
+class procedure TPedido.Pedir(Qtde: Integer; const Tipo: string);
+var
+  I: Integer;
+begin
+  for I := 0 to Qtde - 1 do
+    FPizzas := FPizzas + [TPizzaStore.PedirPizza(Tipo)];
 end;
 
 end.
