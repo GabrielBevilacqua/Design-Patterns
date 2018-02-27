4bc383502d24437ba7dcacbd726f8873654a4013
GabrielBevilacqua <gabriel.scavassa97@gmail.com>
2018-02-27 15:28:59 -0300

    Fabio Salvando

---
+++ Abstract Factory Conversor/JSONToDataSet.pas
@@ -0,0 +1,57 @@
+unit JSONToDataSet;
+
+interface
+
+uses
+  Datasnap.DBClient, UnitConversor, System.JSON, Data.DB, System.SysUtils,
+  System.IOUtils;
+
+type
+  TJSONToDataSet = class
+    class procedure Converter(const caminho: string; DataSet: TClientDataSet);
+  end;
+
+implementation
+
+{ TJSONToDataSet }
+
+class procedure TJSONToDataSet.Converter(const caminho: string;
+  DataSet: TClientDataSet);
+var
+  ListaJSON: TJSONArray;
+  ValorJSON: TJSONValue;
+  ItemJSON: TJSONValue;
+  Field: TField;
+begin
+  try
+    ListaJSON := TJSONObject.ParseJSONValue(TEncoding.ASCII.GetBytes(TFile.ReadAllText(caminho)), 0) as TJSONArray;
+    if ListaJSON.Count > 0 then
+    begin
+      ValorJSON := ListaJSON.Items[0];
+      for ItemJSON in TJSONArray(ValorJSON) do
+      begin
+        Field := TWideStringField.Create(DataSet);
+        Field.Name := '';
+        Field.FieldName := TJSONPair(ItemJSON).JsonString.Value;
+        Field.DataSet := DataSet
+      end;
+    end;
+
+    DataSet.CreateDataSet;
+    for ValorJSON in ListaJSON do
+    begin
+      DataSet.Insert;
+      for ItemJSON in TJSONArray(ValorJSON) do
+      begin
+        DataSet.FieldByName(TJSONPair(ItemJSON).JsonString.Value).Value :=
+          TJSONPair(ItemJSON).JsonValue.Value;
+        Form1.rchTextos.Lines.Add(Format('%s : %s', [TJSONPair(ItemJSON).JsonString.Value, TJSONPair(ItemJSON).JsonValue.Value]));
+      end;
+      DataSet.Post;
+    end;
+  finally
+    ListaJSON.Free;
+  end;
+end;
+
+end.
